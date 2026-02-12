e83747386d909552ba975791a57c86797e568e25
Sanyam33 <purohitsanyam0311@gmail.com>
2026-02-12 13:14:58 +0530

    used pyscopg in url

--- db.py
+++ db.py
@@ -25,8 +25,8 @@ Base = declarative_base()
 #     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 # engine = create_engine(DATABASE_URL)
 
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+# if DATABASE_URL.startswith("postgres://"):
+#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
